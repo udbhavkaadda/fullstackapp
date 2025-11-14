862dedb5bba0ec2e98a9a7b6bd7b27e7100faaa5
udbhav chauhan <udbhavchauhan97@gmail.com>
2025-11-14 16:40:50 +0530

    added aks block

---
+++ modules/aks/variables.tf
@@ -0,0 +1,8 @@
+variable"resource_group_name"{}
+variable"location"{}
+variable"cluster_name"{}
+variable "dns_prefixes" {
+    description = "The DNS prefix for the AKS cluster."
+    type        = string
+    }
+  
